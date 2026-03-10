36496b1996cab14ae215907c122792a9d35d2ae4
Duy <115176150+htdbui@users.noreply.github.com>
2026-03-10 14:21:18 +0000

    git

---
+++ visuals/boxplot/.tmp/build/.tmp/precompile/visualPlugin.d.ts
@@ -0,0 +1,4 @@
+import powerbiVisualsApi from "powerbi-visuals-api";
+import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
+declare var boxplot5789B6ADE08948938C53E34A70C2BC7E: IVisualPlugin;
+export default boxplot5789B6ADE08948938C53E34A70C2BC7E;
